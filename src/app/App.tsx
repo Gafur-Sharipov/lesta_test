ed5c3af6c7ec8dde564a1218409c97fc16680c30
Feyalon <feyalon@localhost.localdomain>
2025-03-30 01:38:44 +0500

    feat: Init app with webpack + react + typescript

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>Welcome to your new React application!</p>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+};
+
+export default App;
